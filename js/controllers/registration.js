5a8d8bcfa81f88d7c6557495757a74c481d57452
Richard Peterson <webbuyrich@gmail.com>
2016-01-03 05:28:18 -0600

    Added registration validation

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,5 +1,13 @@
 myApp.controller('RegistrationController', ['$scope', function($scope){
 
-	$scope.message = "Welcome to my Registration App";
+	$scope.login = function(){
+
+		$scope.message = 'Welcome ' + $scope.user.email;
+	};
+
+	$scope.register = function(){
+
+		$scope.message =  $scope.user.firstname + ' ' + $scope.user.lastname + ' you have successfully registered! Use your email and password to login.';
+	};
 
 }]);
